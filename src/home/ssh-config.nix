9c8d318658b9dbb8dc2cf51f202f9ad1ebd5c806
Antonio Escalera <antonio@defenseunicorns.com>
2026-04-30 13:58:32 -0400

    fix: matchBlocks already inside of programs.ssh

--- src/home/ssh-config.nix
+++ src/home/ssh-config.nix
@@ -6,6 +6,21 @@
 }: {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
+
+    # former enableDefaultConfig = true options
+    matchBlocks."*" = {
+      forwardAgent = false;
+      addKeysToAgent = "no";
+      compression = false;
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      hashKnownHosts = false;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+    };
 
     # Global SSH configuration with RSA support added
     extraConfig = ''
