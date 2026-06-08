52c6622b343594da6cf75549ccecdc21c6be8819
Antonio Escalera <antonio@defenseunicorns.com>
2026-06-08 09:38:04 -0400

    upd: new gpg key
    Signed-off-by: Antonio Escalera <antonio@defenseunicorns.com>

--- src/config.nix
+++ src/config.nix
@@ -2,7 +2,8 @@
   username = "aescalera";
   userfullname = "Antonio Escalera";
   useremail = "antonio@defenseunicorns.com";
-  usersigningkey = "F6F06231397E03C184D461C792FEE6EDB2019086";
+  # usersigningkey = "F6F06231397E03C184D461C792FEE6EDB2019086";
+  usersigningkey = "DD61E292D45B151962C75EE83D651165E430AD73";
   usertimezone = "America/New_York";
   system = "aarch64-darwin";
   hostname = "M4R03MX5V4";
